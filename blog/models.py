bf0c895171b0a6e7c686ce4974c63e0330695cba
Miss Terry Davis <tdavis_909@hotmail.com>
2026-08-03 12:32:11 +0100

    Code it yourself Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from xml.etree.ElementTree import Comment
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -16,3 +18,16 @@ class Post(models.Model):
 content = models.TextField()
 created_on = models.DateTimeField(auto_now_add=True)
 status = models.IntegerField(choices=STATUS, default=0)
+excerpt = models.TextField(blank=True)
+updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
